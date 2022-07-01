99298066b5fb51600e0b6389e06723c08eec88b3
Jonathan Keslin <github@jonathankeslin.com>
2022-07-01 20:28:34 +0000

    Validate colors

---
+++ src/conditions.ts
@@ -0,0 +1,36 @@
+import { localize } from "./localize/localize";
+
+export const LABELS = {
+    'clear-night': localize('conditions.clear'),
+    'cloudy': localize('conditions.cloudy'),
+    'fog': localize('conditions.fog'),
+    'hail': localize('conditions.hail'),
+    'lightning': localize('conditions.thunderstorm'),
+    'lightning-rainy': localize('conditions.thunderstorm'),
+    'partlycloudy': localize('conditions.partlyCloudy'),
+    'pouring': localize('conditions.heavyRain'),
+    'rainy': localize('conditions.rain'),
+    'snowy': localize('conditions.snow'),
+    'snowy-rainy': localize('conditions.mixedPrecip'),
+    'sunny': localize('conditions.sunny'),
+    'windy': localize('conditions.windy'),
+    'windy-variant': localize('conditions.windy'),
+    'exceptional': localize('conditions.clear')
+};
+export const ICONS = {
+    'clear-night': 'weather-night',
+    'cloudy': 'cloudy',
+    'fog': 'fog',
+    'hail': 'hail',
+    'lightning': 'lightning',
+    'lightning-rainy': 'lightning-rainy',
+    'partlycloudy': 'weather-partly-cloudy',
+    'pouring': 'pouring',
+    'rainy': 'rainy',
+    'snowy': 'snowy',
+    'snowy-rainy': 'snowy-rainy',
+    'sunny': 'sunny',
+    'windy': 'windy',
+    'windy-variant': 'windy-variant',
+    'exceptional': 'alert-outline'
+};
