890fc92d5485baf3eaac0ee1bad296f42294d4f6
Paduber <40840023+Paduber@users.noreply.github.com>
2023-10-17 08:50:41 +0500

    Add russian translation (#535)
    * Create ru.json
    
    Add russian translation
    
    * Update localize.ts
    
    * Ensure imported and used locales are sorted
    
    * Setup auto-loc for ru, add to tests
    
    ---------
    
    Co-authored-by: Jonathan Keslin <decompil3d@users.noreply.github.com>
    Co-authored-by: Jonathan Keslin <github@jonathankeslin.com>

--- src/localize/localize.ts
+++ src/localize/localize.ts
@@ -11,6 +11,7 @@ import * as nl from './languages/nl.json';
 import * as pl from './languages/pl.json';
 import * as pt from './languages/pt.json';
 import * as pt_BR from './languages/pt-BR.json';
+import * as ru from './languages/ru.json';
 import * as sk from './languages/sk.json';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -28,6 +29,7 @@ const languages: any = {
   pl,
   pt,
   pt_BR,
+  ru,
   sk,
 };
 
